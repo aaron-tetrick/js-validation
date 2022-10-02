185fb6adad7293771caf15160bfeaec8cf00f1df
Aaron Tetrick <atetrick14102@gmail.com>
2022-10-02 02:04:32 -0400

    Add styling to the form

---
+++ script.js
@@ -0,0 +1,17 @@
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const zip = document.getElementById('zip');
+const pword = document.getElementById('pword');
+const confirm = document.getElementById('confirm');
+
+email.addEventListener('input', e => {
+    console.log(e.target.value)
+
+});
+
+
+
+form.addEventListener('click', e => {
+
+
+});

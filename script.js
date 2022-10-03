0ea8f17013360f3bd97a6be5480ac4d81e98b09d
Aaron Tetrick <atetrick14102@gmail.com>
2022-10-03 04:04:57 -0400

    Add logic for displaying error messages when submitting the form

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const pwordError = document.querySelector('#pword + span.error');
 const confirmError = document.querySelector('#confirm + span.error');
 const form = document.querySelector('form');
 
-console.log(country)
-
 email.addEventListener('input', e => {
     if (email.validity.valid) {
         emailError.textContent = '';
@@ -30,24 +28,57 @@ country.addEventListener('input', e => {
     }
 });
 
+zip.addEventListener('input', e => {
+    if (zip.validity.valid) {
+        zipError.textContent = '';
+        zipError.className = 'error';
+    } else {
+        showError(zip);
+    }
+});
+
+pword.addEventListener('input', e => {
+    if (pword.validity.valid) {
+        pwordError.textContent = '';
+        pwordError.className = 'error';
+    } else {
+        showError(pword);
+    }
+});
+
+confirm.addEventListener('input', e => {
+    if (confirm.validity.valid) {
+        confirmError.textContent = '';
+        confirmError.className = 'error';
+    } else {
+        showError(confirm);
+    }
+});
+
+
 function showError(type) {
-    console.log(type);
     if (type.validity.valueMissing) {
             document.querySelector(`#${type.name} + span.error`).textContent = 'Please enter in a value';
-       
     } else if (type.validity.typeMismatch) {
         if(type.name === 'email') {
         emailError.textContent = 'Entered value needs to be an email';
         }
+    } else if (type.validity.tooShort) {
+        if (type === pword) {
+        pwordError.textContent = `Password must be at least ${pword.minLength} characters long.`
+        } 
+        if (type === zip) {
+          zipError.textContent = `Zip Code must be ${zip.minLength} characters long`;
+        }
     }
 };
 
-
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log(e);
-    if (!email.validity.valid) {
-        showError(email);
+    const inputs = Array.from(document.querySelectorAll('.input'));
+    for (i=0; i < inputs.length; i++) {
+        if (!inputs[i].validity.valid) {
         e.preventDefault();
+        showError(inputs[i]);
+        }
     }
 });
